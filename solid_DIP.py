400fa2b343a2deb236a47c2593567852794ae2ba
zeal-cloud <mahmoudessam.me@gmail.com>
2022-07-04 22:44:05 +0200

    final commit

--- solid_DIP.py
+++ solid_DIP.py
@@ -13,9 +13,12 @@ class Person:
     def __init__(self, name):
         self.name = name
 
+
 class Relationship_Browser:
     @abstractmethod
-    def find_all_children_of(self,name):pass
+    def find_all_children_of(self, name): pass
+
+
 class Relationships(Relationship_Browser):
     def __init__(self):
         self.relations = []
@@ -27,19 +30,19 @@ class Relationships(Relationship_Browser):
         self.relations.append(
             (child, Relationship.CHILD, parent)
         )
-    def find_all_children_of(self,name):
+
+    def find_all_children_of(self, name):
         for r in self.relations:
-            if r[0]==name and r[1]==Relationship.PARENT:
-                yield r[2].name   
-  
+            if r[0] == name and r[1] == Relationship.PARENT:
+                yield r[2].name
+
+
 class Research:
-    def __init__(self,browser):
+    def __init__(self, browser):
         for p in browser.find_all_children_of('John'):
-            print(f'John has a child called {p}') 
+            print(f'John has a child called {p}')
     # def __init__(self,relationships):
     #     relations = relationships.relations
     #     for r in relations:
     #         if r[0].name =='John'and r[1]==Relationship.PARENT:
     #             print(f'John has a child called {r[2].name}')
-    
-       
